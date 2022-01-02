1a834895ff4ce9ee141d91426f3a1205eba2aab6
富安 恭平 <49761051+tomykyou@users.noreply.github.com>
2022-01-02 12:28:08 +0900

    devise導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  resources :mainnote
-  root 'mainnote#top'
 #  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-#  devise_for :users
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  
-#  get 'sessions/new'
-#  root "mainnote#top"
+  get 'sessions/new'
+  root "mainnote#top"
 #  get 'mainnnote/mypage', to: 'mainnote#mypage'
 #  get 'mainnnote/about', to: 'mainnote#about'
 #  get 'mainnnote/usershow', to: 'mainnote#usershow'
