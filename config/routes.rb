4be33934c101b80f9143f1e2c8cfaf40c07ba28b
富安 恭平 <49761051+tomykyou@users.noreply.github.com>
2022-01-02 12:13:52 +0900

    トップページの表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :mainnote
+  root 'mainnote#top'
 #  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 #  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
