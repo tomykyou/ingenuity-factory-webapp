46eea4801e92f44d579c5d633d20854da8527e6f
富安 恭平 <49761051+tomykyou@users.noreply.github.com>
2022-01-08 19:11:39 +0900

    bootstrapの適用

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,6 @@ module IngenuityFactoryWebapp
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-
+    config.i18n.default_locale = :ja 
   end
 end
