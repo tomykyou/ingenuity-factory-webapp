7fe9309425305b1ecdf9dddfba21baaa706f9857
富安 恭平 <49761051+tomykyou@users.noreply.github.com>
2022-01-14 00:35:45 +0900

    レイアウト調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,12 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
+    before_action :set_host
 
+    def set_host
+      Rails.application.routes.default_url_options[:host] = request.host_with_port
+    end
+    
     def configure_permitted_parameters
         added_attrs = [:username, :image, :occupation, :award, :password, :password_confirmation, :remove_image]
         devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
